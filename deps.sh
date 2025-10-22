284307da8f2e5c4b3f949f4e7fadd20ff1cfd5c5
Lkaetsu <111883767+Lkaetsu@users.noreply.github.com>
2025-10-22 19:55:15 -0300

    Set up Python virtual environment and run Flask app
    Added commands to set up a Python virtual environment and run a Flask application.

--- deps.sh
+++ deps.sh
@@ -10,3 +10,7 @@ systemctl start mysql.service
 # exit
 # mysql_secure_installation
 systemctl restart mysql
+python -m venv /vagrant/.venv
+source /vagrant/.venv/bin/activate
+pip install -r /vagrant/requirements.txt
+nohup flask run --host=0.0.0.0 > /vagrant/logs/app.log 2>&1 &

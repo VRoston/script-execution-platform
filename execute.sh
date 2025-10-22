4f147fab849242f23d728743453dd85e025d8d17
Lkaetsu <111883767+Lkaetsu@users.noreply.github.com>
2025-10-22 18:52:52 -0300

    Changing the filename to a variable and putting both stdout and stderr in the output file

--- execute.sh
+++ execute.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 echo "+cpu +cpuset" > /sys/fs/cgroup/cgroup.subtree_control
-mkdir /sys/fs/cgroup/execute1
-echo $1 > /sys/fs/cgroup/execute1/cpuset.cpus
-echo $2 > /sys/fs/cgroup/execute1/cpu.weight
-echo "$3 100000" > /sys/fs/cgroup/execute1/cpu.max
-echo $$ > /sys/fs/cgroup/freezer/sub/cgroup.procs
+mkdir /sys/fs/cgroup/$1
+echo $2 > /sys/fs/cgroup/$1/cpuset.cpus
+echo $3 > /sys/fs/cgroup/$1/cpu.weight
+echo "$4 100000" > /sys/fs/cgroup/$1/cpu.max
+echo $$ > /sys/fs/cgroup/$1/sub/cgroup.procs
 unshare -C --mount-proc /bin/bash
-gcc -g -o execute1 execute1.c && ./execute1 > output.txt
+gcc -g -o $1 $1.c && ./$1 > output.txt 2>&1
 exit
+

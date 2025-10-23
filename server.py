07d89a436846a0316f45f268ac588773b5b49002
VRoston <victor.mr1@puccampinas.edu.br>
2025-10-22 22:20:26 -0300

    feat: get cpu front

--- server.py
+++ server.py
@@ -1,16 +1,23 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 import subprocess
 
 app = Flask(__name__)
 
-@app.route('/run-script', methods=['GET'])
+@app.route('/run-script', methods=['POST'])
 def run_script():
     try:
-        # Executa o script e captura a saída
-        result = subprocess.run(['/vagrant/execute.sh'], capture_output=True, text=True, check=True)
+        data = request.get_json()
+        cpu = data.get('cpu')
+        cpuUsage = (data.get('cpuUsage') * 1000)
+        weight = data.get('weight')
+        # Monta o comando com as variáveis recebidas
+        cmd = ["/vagrant/execute.sh", "execute", str(cpu), str(weight), str(cpuUsage)]
+        result = subprocess.run(cmd, capture_output=True, text=True, check=True)
         return jsonify({"output": result.stdout.strip()})
     except subprocess.CalledProcessError as e:
         return jsonify({"error": e.stderr.strip()}), 500
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
